a9fc759ac96f5ca5990ed99375b0d992bd199ee4
Tyler Seefeldt <tseefeldt@gmail.com>
2025-01-27 14:58:21 -0700

    Revised code for C06

--- Assignments/C06/main.c
+++ Assignments/C06/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
     int i;
     unsigned long long a;
 
-    scanf("%lld", &a);
+    if (argc <= 2){
+        a = atoi(argv[1]);
+    }
 
     i = a;
     while (i > 1) {
