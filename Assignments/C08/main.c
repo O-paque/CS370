d8b5412b6d60db6691329b1bf10467984a95e4cb
Tyler Seefeldt <tseefeldt@gmail.com>
2025-02-04 17:55:54 -0700

    Autograder is timing out, maybe not accounting for all whitespaces and I cant import and use isspace

--- Assignments/C08/main.c
+++ Assignments/C08/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+int notSpace (char c);
+
 int main(int argc, char **argv) {
     char c[1000];
     char vowels[10] = {'a','A','e','E','i','I','o','O','u','U'};
     char h;
     int counter = 0;
 
-    while (h != ' ') {
+    while (notSpace(h)) {
         scanf("%c", &h);
         for (int i = 0; i < sizeof(vowels); i++){
             if (vowels[i] == h) {
@@ -15,5 +17,16 @@ int main(int argc, char **argv) {
         }
     }
     printf("%d\n", counter);
+    
     return 0;
+}
+
+int notSpace(char c) {
+    char s[6] = {' ', '\t', '\n', '\v', '\f', '\r'};
+    for (int i = 0; i < sizeof(s); i++){
+        if (s[i] == c){
+            return 0;
+        }
+    }
+    return 1;
 }
