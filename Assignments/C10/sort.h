62121097385b54a9ec7801c62390930aca7f755c
Tyler Seefeldt <tseefeldt@gmail.com>
2025-02-04 19:20:08 -0700

    Removed sort source file per assignment requirements

--- Assignments/C10/sort.h
+++ Assignments/C10/sort.h
@@ -1,6 +1,16 @@
 #ifndef SORT_H
 #define SORT_H
 
-void sort (int arr[], int size);
+void sort (int arr[], int size) {
+    for (int i = 0; i < size; i++){
+        for (int j = i+1; j < size; j++){
+            if (arr[j] < arr[i]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
 
 #endif
